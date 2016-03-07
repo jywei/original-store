17a5f56251a01efc12278e372e9f20b00c9f8b3f
Roy Wei <roywjy@gmail.com>
2016-03-07 16:47:05 +0800

    add product upload page and backend for admin user

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,27 @@
 class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      flash[:success] = "The product is successfully saved"
+      redirect_to admin_products_path
+    else
+      flash[:error] = "The product is not saved, try again"
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :body, :quantity, :price)
+  end
 end
