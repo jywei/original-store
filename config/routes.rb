2880c4d6f32903b960097c70c4ae3988474abfc2
Roy Wei <roywjy@gmail.com>
2016-03-08 21:42:03 +0800

    build the shopping cart showing page

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#index'
 
+  resources :carts
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
