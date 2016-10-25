979151b512ae0b383502bdd5888d09a1b5b83db1
Roy Wei <roywjy@gmail.com>
2016-10-25 19:50:11 +0800

    test user create function successful

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe User, type: :model do
   it "is the first test" do
     expect(User).to be_present
   end
+
+  it "test the creation of a user" do
+    user = User.create(email: "user@test.com",
+                       password: "12345678",
+                       password_confirmation: "12345678")
+    expect(user).to be_valid
+  end
 end
