ddc291281876d06a2ab23eee626163346e209e0b
Roy Wei <roywjy@gmail.com>
2016-10-25 21:25:53 +0800

    update controller spec

--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -1,31 +1,121 @@
 require 'rails_helper'
 
 RSpec.describe Admin::ProductsController, type: :controller do
-  subject(:admin_user) { User.create(email: "admin@example.com",
-                                 password: "12345678",
-                                 password_confirmation: "12345678",
-                                 is_admin: true) }
-  subject(:normal_user) { User.create(email: "user@example.com",
-                                  password: "12345678",
-                                  password_confirmation: "12345678",
-                                  is_admin: false) }
+  subject(:admin_user) { User.create(email: "admin@test.com", password: "12345678", password_confirmation: "12345678", is_admin: true) }
+  subject(:normal_user){ User.create(email: "user@test.com", password: "12345678", password_confirmation: "12345678") }
+  subject(:product)    { Product.create(title: "macbook", price: "60000", quantity: "5", body: "Apple")}
 
   describe "GET index" do
-    before { sign_in :user, admin_user }
-    before { get :index }
+    context "login admin_user" do
+      before { sign_in :user, admin_user }
+      before { get :index }
 
-    it { expect(response).to be_success }
-    it { expect(response.header['Content-Type']).to include "text/html" }
+      it { expect(response).to be_success }
+      it { expect(response.header['Content-Type']).to include "text/html" }
+    end
 
-    context "json" do
+    context "login normal_user" do
+      before { sign_in :user, normal_user }
+      before { get :index }
+
+      it { expect(response).to redirect_to root_path }
+    end
+
+    context "login admin_user with json" do
+      before { sign_in :user, admin_user }
       before { get :index, format: :json }
+
+      it { expect(response).to be_success }
       it { expect(response.header['Content-Type']).to include "application/json" }
+    end
+
+    context "login normal_user with json" do
+      before { sign_in :user, normal_user }
+      before { get :index, format: :json }
+
+      it { expect(response).to redirect_to root_path }
     end
   end
 
-  describe "GET new"
-  describe "POST create"
-  describe "GET edit"
-  describe "PATCH update"
+  describe "GET new" do
+    context "login admin_user" do
+      before { sign_in :user, admin_user }
+      before { get :new }
 
+      it { expect(response).to be_success }
+      it { expect(response.header['Content-Type']).to include "text/html" }
+    end
+
+    context "login normal_user" do
+      before { sign_in :user, normal_user }
+      before { get :new }
+
+      it { expect(response).to redirect_to root_path }
+    end
+  end
+
+  describe "GET edit" do
+    context "login admin_user" do
+      before { sign_in :user, admin_user }
+      before { get :edit, id: product.id }
+
+      it { expect(response).to be_success }
+      it { expect(response.header['Content-Type']).to include "text/html" }
+    end
+
+    context "login normal_user" do
+      before { sign_in :user, normal_user }
+      before { get :edit, id: product.id }
+
+      it { expect(response).to redirect_to root_path }
+    end
+  end
+
+  describe "POST create" do
+    context "login admin_user" do
+      before { sign_in :user, admin_user }
+      before { post :create, product: { title: "macbook",
+                                        price: "60000",
+                                        quantity: "5",
+                                        body: "Apple" }}
+
+      it { expect(Product.count).to eq 1 }
+      it { is_expected.to redirect_to admin_products_path }
+
+      it "create product fail" do
+        post :create, product: { price: "60000",
+                                 quantity: "5",
+                                 body: "Apple" }
+        is_expected.to render_template :new
+      end
+    end
+
+    context "login normal_user" do
+      before { sign_in :user, normal_user }
+      before { post :create, product: { title: "macbook",
+                                        price: "60000",
+                                        quantity: "5",
+                                        body: "Apple" }}
+
+      it { expect(response).to redirect_to root_path }
+    end
+  end
+
+  describe "PUT/PATCH update" do
+    context "login admin_user" do
+      before { sign_in :user, admin_user }
+      before { patch :update, id: product.id, product: { title: "Macbook Pro" } }
+
+      it { is_expected.to redirect_to admin_products_path }
+      it { expect(response.header['Content-Type']).to include "text/html" }
+      it { expect(Product.find(product.id).title).to eq "Macbook Pro" }
+    end
+
+    context "login normal_user" do
+      before { sign_in :user, normal_user }
+      before { patch :update, id: product.id, product: { title: "Macbook Pro" } }
+
+      it { expect(response).to redirect_to root_path }
+    end
+  end
 end
