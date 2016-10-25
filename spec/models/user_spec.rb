4730a4e23729fd62a561918e0a5e45ccc7f547f2
Roy Wei <roywjy@gmail.com>
2016-10-25 19:54:57 +0800

    use subject and let function to refactor the test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:user) { User.create(email: email,
+                               password: password,
+                               password_confirmation: password_confirmation) }
+
+  let(:email) { "user@test.com" }
+  let(:password) { "12345678" }
+  let(:password_confirmation) { "12345678" }
+
   it "is the first test" do
-    expect(User).to be_present
+    is_expected.to be_present
   end
 
   it "test the creation of a user" do
-    user = User.create(email: "user@test.com",
+    is_expected.to be_valid
+  end
+
+  it "test user creation failure" do
+    user = User.create(email: "",
                        password: "12345678",
                        password_confirmation: "12345678")
-    expect(user).to be_valid
+    expect(user).to be_invalid
   end
 end
