5ffbe96f0af0b7a6936c8de0c88421e2d5217557
Roy Wei <roywjy@gmail.com>
2016-10-27 23:50:05 +0800

    refactor admin products controller index action

--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -1,53 +1,39 @@
+
 require 'rails_helper'
 
 RSpec.describe Admin::ProductsController, type: :controller do
-  # subject(:admin_user) { User.create(email: Faker::Internet.email,
-  #                                    password: "12345678",
-  #                                    password_confirmation: "12345678",
-  #                                    is_admin: true) }
-
-  # subject(:normal_user) { User.create(email: Faker::Internet.email,
-  #                                    password: "12345678",
-  #                                    password_confirmation: "12345678") }
-
-  subject(:admin_user)  { create(:admin_user) }
-  subject(:normal_user) { create(:user) }
-
-  # subject(:product) { Product.create(title: "macbook",
-  #                                    price: "60000",
-  #                                    quantity: "5",
-  #                                    body: "Apple")}
-  subject(:product) { create(:product) }
+  subject(:user)          { create(:admin_user) }
+  subject(:admin_user)    { create(:admin_user) }
+  subject(:normal_user)   { create(:user) }
+  subject(:product)       { create(:product) }
 
   describe "GET index" do
-    context "login admin_user" do
-      before { sign_in :user, admin_user }
+    before { sign_in :user, user }
+
+    context "format html" do
       before { get :index }
 
       it { expect(response).to be_success }
       it { expect(response.header['Content-Type']).to include "text/html" }
+
+      context "normal user" do
+        let(:user) { create(:user) }
+
+        it { expect(response).to redirect_to root_path }
+      end
     end
 
-    context "login normal_user" do
-      before { sign_in :user, normal_user }
-      before { get :index }
-
-      it { expect(response).to redirect_to root_path }
-    end
-
-    context "login admin_user with json" do
-      before { sign_in :user, admin_user }
+    context "format json" do
       before { get :index, format: :json }
 
       it { expect(response).to be_success }
       it { expect(response.header['Content-Type']).to include "application/json" }
-    end
 
-    context "login normal_user with json" do
-      before { sign_in :user, normal_user }
-      before { get :index, format: :json }
+      context "normal user" do
+        let(:user) { create(:user) }
 
-      it { expect(response).to redirect_to root_path }
+        it { expect(response).to redirect_to root_path }
+      end
     end
   end
 
